07a2abc17f139822f0c98afb035a5aceaa5ef6c7
Eduardo Galindez Cintron <eduardoGalindezcintron@Eduardos-MacBook-Pro.local>
2015-10-04 08:33:16 -0400

    added funcionality

--- server.js
+++ server.js
@@ -40,6 +40,28 @@ app.post('/comments.json', function(req, res) {
   });
 });
 
+app.get('/helpers.json', function(req, res) {
+  fs.readFile('helpers.json', function(err, data) {
+    res.setHeader('Cache-Control', 'no-cache');
+    res.json(JSON.parse(data));
+  });
+});
+
+app.get('/helpers', function(req, res) {
+  res.sendfile(__dirname+'/public/helper.html');
+});
+
+app.post('/helpers.json', function(req, res) {
+  fs.readFile('helpers.json', function(err, data) {
+    var comments = JSON.parse(data);
+    comments.push(req.body);
+    fs.writeFile('helpers.json', JSON.stringify(comments, null, 4), function(err) {
+      res.setHeader('Cache-Control', 'no-cache');
+      res.json(comments);
+    });
+  });
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Server started: http://localhost:' + app.get('port') + '/');
